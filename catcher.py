a827bca1498835e478fb17e71248f714bdaab5e7
Camia Trefs Sebastian <logicalbrainstudio@gmail.com>
2023-03-02 22:39:55 -0300

    Get data from Sheets w/o header

--- catcher.py
+++ catcher.py
@@ -1,14 +1,12 @@
 import pandas as pd
+import requests
 
 # WE PROCESS THE TXT FILE WITH THE GOOGLE SHEETS ID IN IT
-with open('sheet_id.txt') as f:
-    sheet_ID = f.readlines()
-
-sheet_name = "AAPL"
-
-# WE CALL THE URL OF THE GOOGLE SHEET WITH THE ID
-url = f'https://docs.google.com/spreadsheets/d/{sheet_ID}/gviz/tq?tqx=out:csv&sheet=/{sheet_name}'
-
-# CATCH 'EM ALL!
-url_catcher = pd.read_csv(url)
-print(url_catcher.head())
+with open('sheet_id.txt', "r") as f:
+    sheet_ID = f.readline().strip()
+    while True:
+        # WE CALL THE URL OF THE GOOGLE SHEET WITH THE ID
+        url = f'https://docs.google.com/spreadsheets/d/{sheet_ID}/gviz/tq?tqx=out:csv&sheet=/'
+        # CATCH 'EM ALL!
+        url_catcher = pd.read_csv(url, header=1)
+        print(url_catcher.head())
