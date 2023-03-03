2d73ed88f6214ea5cf6841ce10fbc9a23863c45c
Camia Trefs Sebastian <logicalbrainstudio@gmail.com>
2023-03-02 23:14:58 -0300

    Testings

--- catcher.py
+++ catcher.py
@@ -2,15 +2,14 @@ import pandas as pd
 import requests
 
 # WE PROCESS THE TXT FILE WITH THE GOOGLE SHEETS ID IN IT
-def get_sheet():
+def get_sheet(x, y):
     with open('sheet_id.txt', "r") as f:
         sheet_ID = f.readline().strip()
         while True:
             # WE CALL THE URL OF THE GOOGLE SHEET WITH THE ID
             url = f'https://docs.google.com/spreadsheets/d/{sheet_ID}/gviz/tq?tqx=out:csv&sheet=/'
             # CATCH 'EM ALL!
-            url_catcher = pd.read_csv(url, header=1)
-
-            return url_catcher.head()
+            url_catcher = pd.read_csv(url)
+            return url_catcher.iloc[x, y]
         
-print(get_sheet())
+print(get_sheet(1, 1))
