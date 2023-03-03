be3fb7ee909f47586d57b717c74e8ff90f72f427
Camia Trefs Sebastian <logicalbrainstudio@gmail.com>
2023-03-02 22:46:45 -0300

    Added func to loader

--- catcher.py
+++ catcher.py
@@ -2,11 +2,15 @@ import pandas as pd
 import requests
 
 # WE PROCESS THE TXT FILE WITH THE GOOGLE SHEETS ID IN IT
-with open('sheet_id.txt', "r") as f:
-    sheet_ID = f.readline().strip()
-    while True:
-        # WE CALL THE URL OF THE GOOGLE SHEET WITH THE ID
-        url = f'https://docs.google.com/spreadsheets/d/{sheet_ID}/gviz/tq?tqx=out:csv&sheet=/'
-        # CATCH 'EM ALL!
-        url_catcher = pd.read_csv(url, header=1)
-        print(url_catcher.head())
+def get_sheet():
+    with open('sheet_id.txt', "r") as f:
+        sheet_ID = f.readline().strip()
+        while True:
+            # WE CALL THE URL OF THE GOOGLE SHEET WITH THE ID
+            url = f'https://docs.google.com/spreadsheets/d/{sheet_ID}/gviz/tq?tqx=out:csv&sheet=/'
+            # CATCH 'EM ALL!
+            url_catcher = pd.read_csv(url, header=1)
+
+            return url_catcher.head()
+        
+print(get_sheet())
